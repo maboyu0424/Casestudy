8e5b508837f826186ca1cf75d14414a7acd6393e
maboyu0424 <55783251+maboyu0424@users.noreply.github.com>
2022-12-08 15:40:41 +0800

    Update main.dart

--- casestudy/web/main.dart
+++ casestudy/web/main.dart
@@ -28,7 +28,9 @@ void main() {
     var day = daytime.valueAsDate.day;
 
     var venueNumber = int.parse((venue.value).toString());
-    var onlyNumber = venueNumber * 10000000 + sth * 100000 + edh * 10 + day;
+    var onlyNumber =
+        venueNumber * 100000000 + 30000000 + sth * 10000 + edh * 100 + day;
+
     print(onlyNumber);
     var t = true;
     var r = true;
@@ -41,6 +43,28 @@ void main() {
         print("false");
       }
     }
+    var only1 = onlyNumber.toString().replaceFirst("3", "f");
+    var g = only1.split("f");
+    var k = int.parse(g[1]);
+
+    for (var i = 0; i < bookingtable.length; i++) {
+      var j = ((bookingtable[i].toString()).replaceFirst("3", "f")).split("f");
+      var l = int.parse(j[1]);
+      print("here is $l");
+    }
+
+    // var g = onlyNumber.toString().split("3");
+    // var k = int.parse(g[1]);
+
+    // for (var i = 0; i < bookingtable.length; i++) {
+    //   var j = bookingtable[i].toString().split("3");
+    //   var l = int.parse(j[1]);
+    //   var z = ((l / 10000).toString());
+
+    //   print("here is $l");
+    //   print("here is ${g}");
+    //   print(z);
+    // }
 
     if (sth < edh) {
       if (sth >= 8 && edh <= 23) {
